09ff78931d6baa3dad97cec80f8a93b7eb7ee6f8
Alexandre Campo <alexandre.campo@gmail.com>
2017-03-02 18:15:06 +0100

    added missing install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# check for dependencies
+sudo apt-get install make g++ libgsl0-dev libtinyxml-dev libboost-program-options-dev libbullet-dev premake4 chipmunk-dev libopenscenegraph-dev freeglut3-dev
+
+# build
+premake4 gmake
+make
+premake4 debpackage
+
+#install
+sudo dpkg -i libfamous-3.0.deb
